824ad70d50d5e3c2bd6b2daa11a8a841a87bca06
abhimaniu041-sudo <abhimaniu041@gmail.com>
2026-04-29 18:46:37 +0530

    Update gemini_service.dart

--- lib/services/gemini_service.dart
+++ lib/services/gemini_service.dart
@@ -1,17 +1,12 @@
 import 'package:google_generative_ai/google_generative_ai.dart';
 
 class GeminiService {
-  // Yahan apni asli API Key dalo
-  static const String apiKey = "YOUR_ACTUAL_API_KEY"; 
+  static const String apiKey = "YOUR_API_KEY"; 
 
   static Future<String> getAiResponse(String prompt) async {
-    try {
-      final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: apiKey);
-      final content = [Content.text(prompt)];
-      final response = await model.generateContent(content);
-      return response.text ?? "No response from AI";
-    } catch (e) {
-      return "AI Error: $e";
-    }
+    final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: apiKey);
+    final content = [Content.text(prompt)];
+    final response = await model.generateContent(content);
+    return response.text ?? "No response";
   }
 }
