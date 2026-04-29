1d12e99b186361bf41a53df51a49de3cf2db696e
abhimaniu041-sudo <abhimaniu041@gmail.com>
2026-04-29 17:18:52 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+// Baaki imports...
 
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Agar Firebase use kar rahe ho toh initialize yahan hoga
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TriVerse',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(), // Aapka premium dark theme
+      home: const SplashScreen(), // Ya jo bhi aapka pehla screen hai
+    );
+  }
+}
